f024f93a575f668651438469e954701075de4cd2
sun <743685264@qq.com>
2023-12-21 11:01:33 +0800

    这是没有格式化的代码

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+
+const noFormat =()=>
+{
+    console.log('这是没有格式化但却提交的代码');
+}
 const app = createApp(App)
 
 app.use(createPinia())
