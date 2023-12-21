d5cc0a6f2993aedf6641afedca43b5f05306d709
sun <743685264@qq.com>
2023-12-21 14:52:40 +0800

    加入commitlint

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-const str = '我爱你祖国'
-for (let i = 100; i < 100; i++) {
-  console.log(i)
-}
+
 </script>
 
 <template>
@@ -12,12 +9,5 @@ for (let i = 100; i < 100; i++) {
 </template>
 
 <style scoped lang="scss">
-.box {
-  width: 600px;
-  height: 400px;
-  background: black;
-  h1 {
-    color: white;
-  }
-}
+
 </style>
