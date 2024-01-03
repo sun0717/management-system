fc8bc79472552b8e6fe3e5495f18a024d84fc140
sun <743685264@qq.com>
2024-01-03 21:41:04 +0800

    feat: SPU模块部分完成

--- src/api/product/spu/index.ts
+++ src/api/product/spu/index.ts
@@ -1,10 +1,11 @@
 import request from '@/utils/request'
 import type { HasSpuResponseData } from './type'
 enum API {
-    // 获取已有的SPU的数据
-    HASSPU_URL = '/admin/product/', //{page}/{limit}
-
+  // 获取已有的SPU的数据
+  HASSPU_URL = '/admin/product/' //{page}/{limit}
 }
 
-export const reqHasSpu = (page: number, limit: number, category3Id: string|number) => request.get<any, HasSpuResponseData>(API.HASSPU_URL + `${page}/${limit}?category3Id=${category3Id}`);
-
+export const reqHasSpu = (page: number, limit: number, category3Id: string | number) =>
+  request.get<any, HasSpuResponseData>(
+    API.HASSPU_URL + `${page}/${limit}?category3Id=${category3Id}`
+  )
